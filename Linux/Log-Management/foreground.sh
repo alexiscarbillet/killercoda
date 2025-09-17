33901d98fd8c6cbeec3fe4e86eca7634fd46d22d
ALEXIS CARBILLET <alexiscarbillet@users.noreply.github.com>
2025-09-17 09:05:30 -0400

    Update foreground.sh

--- Linux/Log-Management/foreground.sh
+++ Linux/Log-Management/foreground.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create a large file to simulate disk usage
-dd if=/dev/zero of=/var/log/bigfile bs=1M count=12000 status=none
+dd if=/dev/zero of=/var/log/biglog bs=1M count=12000 status=none
 
 # Print a message so the user knows what happened
 echo "A large file was created to simulate a fuller disk."
