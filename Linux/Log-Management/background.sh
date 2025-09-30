bd3d3b60f7e6c018ab9c7a2e8c6dc18187d1adf1
ALEXIS CARBILLET <alexiscarbillet@users.noreply.github.com>
2025-09-30 13:05:05 -0400

    Update background.sh

--- Linux/Log-Management/background.sh
+++ Linux/Log-Management/background.sh
@@ -2,6 +2,3 @@
 
 # Create a large fake log file to fill disk space
 fallocate -l 10G /var/log/biglog.log || dd if=/dev/zero of=/var/log/biglog.log bs=1M count=12000
-
-# Keep container running
-tail -f /dev/null
